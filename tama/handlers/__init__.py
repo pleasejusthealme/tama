0bc4939eb2e0e8e725ca627380b7918d72818b28
sam! <120393338+pleasejusthealme@users.noreply.github.com>
2026-01-05 13:17:32 +0400

    Delete tama directory

--- tama/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Dispatcher
-from .start import register_start_handlers
-from .game import register_game_handlers
-
-def register_handlers(dp: Dispatcher):
-    register_start_handlers(dp)
-    register_game_handlers(dp)
